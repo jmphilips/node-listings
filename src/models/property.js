a0290a2bb162d704c25e6b7a454f8387fd252e69
jmphilips <jmichaelphilips@gmail.com>
2019-05-17 20:43:10 -0400

    uh

--- src/models/property.js
+++ src/models/property.js
@@ -1,4 +1,4 @@
-import { Schema, model } from 'mongoose'
+import { model } from 'mongoose'
 import PropertySchema from '../schemas/propertySchema'
 
 const Property = model('Property', PropertySchema)
